70ea0e6d3f3fbcfab82dfa72588aca26234aec36
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2025-01-24 15:33:46 +0200

    Update auth form

--- auth.php
+++ auth.php
@@ -13,20 +13,20 @@ if (($_SERVER['REQUEST_METHOD'] === 'POST') && !empty($_POST['email'])) {
     $email = trim($_POST['email']);
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         if (($_SESSION['user_id'] = $db->getUser($email)))
-            $body = 'You are successfully logged in! <a href="/">Go to Homepage</a>';
+            $body = 'You are successfully logged in! <br><br><a href="/" class="btn btn-secondary">Go to Homepage</a>';
         elseif ($_SESSION['user_id'] = $db->setUser($email))
-            $body = 'You are successfully authorized! <a href="/">Go to Homepage</a>';
+            $body = 'You are successfully authorized! <br><br><a href="/" class="btn btn-secondary">Go to Homepage</a>';
         else {
-            $body = 'The user is not found! <a href="/auth.php">Try again</a>';
+            $body = 'The user is not found! <a href="/auth.php" class="btn btn-secondary">Try again</a>';
             unset($_SESSION['user_id']);
         }
     }
     else
-        $body = 'Please enter a valid email address! <a href="/auth.php">Try again</a>';
+        $body = 'Please enter a valid email address! <a href="/auth.php" class="btn btn-secondary">Try again</a>';
 }
 else {
     $body = <<<EOT
-        <form action="auth.php" method="post">
+        <form action="/auth.php" method="post">
             <label for="email">Email:</label><br>
             <input type="email" name="email" placeholder="Enter your email" required><br>
             <button type="submit">Authorize</button><br><br>
