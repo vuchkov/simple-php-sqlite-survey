2cf9693c1a333924388588707274fb18e614df4f
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2025-01-24 13:44:38 +0200

    Update authorization

--- auth.php
+++ auth.php
@@ -7,20 +7,22 @@ if (!empty($_SESSION['user_id'])) {
 }
 
 include_once 'config.php';
+$db = new DB();
 
 if (($_SERVER['REQUEST_METHOD'] === 'POST') && !empty($_POST['email'])) {
     $email = trim($_POST['email']);
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $db = new DB();
         if (($_SESSION['user_id'] = $db->getUser($email)))
             $body = 'You are successfully logged in! <a href="/">Go to Homepage</a>';
         elseif ($_SESSION['user_id'] = $db->setUser($email))
             $body = 'You are successfully authorized! <a href="/">Go to Homepage</a>';
-        else unset($_SESSION['user_id']);
+        else {
+            $body = 'The user is not found! <a href="/auth.php">Try again</a>';
+            unset($_SESSION['user_id']);
+        }
     }
-    else {
+    else
         $body = 'Please enter a valid email address! <a href="/auth.php">Try again</a>';
-    }
 }
 else {
     $body = <<<EOT
@@ -32,6 +34,6 @@ else {
 EOT;
 }
 
-$page = new Page('Authorize', $body ?? '');
+$page = new Page('Authorize', $body);
 $page->render();
 
